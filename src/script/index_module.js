b1fd219593b362250ffa3b14824154cba8623465
guoziyuan <284605730@qq.com>
2020-09-28 22:18:43 +0800

    第三次提交

--- src/script/index_module.js
+++ src/script/index_module.js
@@ -1,12 +1,84 @@
 define([], function() {
     return {
         init: function() {
-            console.log(1);
+
+            //二级菜单移入鼠标显示，移除隐藏
+            const $location = $('.header .header-location');
+            const $wrap = $('.header .header-location .city-header-wrap');
+            const $span = $('.header .header-location .city-header-wrap .city-header .list-other .name-city')
+            $location.on('mouseover', function() {
+                $(this).addClass('active');
+                $wrap.show();
+                // $.each(data, (index, value) => {
+                //     $location.append(`<span index=${index}>${value.name}</span>`)
+                // });
+                // console.log(item);
+            });
+            $location.on('mouseout', function() {
+                $wrap.hide();
+            });
+
+            //轮播图
+            const $img = $('.banner ul li');
+            const $circle = $(".banner ol li");
+            const $right = $(".banner .right");
+            const $left = $(".banner .left");
+            const $banner = $(".banner")
+            let num = 0;
+            let timer = '';
+            $circle.eq(0).addClass("active").siblings().removeClass("active");
+            start();
+            $circle.on('mouseover', function() {
+                num = $(this).index();
+                animate();
+            });
+            $banner.on('mouseover', () => {
+                clearTimeout(timer);
+            });
+            $banner.on("mouseout", () => {
+                start();
+            })
+            $right.on('click', function() {
+                num++;
+                if (num == 3) {
+                    num = 0
+                }
+                animate();
+            });
+            $left.on('click', function() {
+                num--;
+                if (num == -1) {
+                    num = 1
+                }
+                animate();
+            });
+
+            function start() {
+                timer = setInterval(function() {
+                    num++;
+                    if (num == 3) {
+                        num = 0
+                    }
+                    animate();
+
+                }, 2000);
+            }
+
+            function animate() {
+                $img.eq(num).fadeIn("slow").siblings().stop(true, true).fadeOut("slow");
+                $circle.eq(num).addClass("active").siblings().removeClass("active");
+            }
+            //点击回到顶部
+            var $top = $('.sidebox .backtop')
+            $top.on('click', function() {
+                $(window).scrollTop(0);
+                return false;
+            });
+            //渲染
             $.ajax({
-                url: 'http://localhost/damai/php/damaiindexli.php',
+                url: 'http://192.168.13.71/damai/php/damaiindexli.php',
                 dataType: 'json'
             }).done(function(data) {
-                console.log(data);
                 let $strhtml = '';
                 $.each(data, function(index, value) {
                     $strhtml += `<li>    
@@ -24,7 +96,6 @@ define([], function() {
                         </li>
                     `;
                 });
-                console.log($strhtml);
                 $(".box-right").html(($strhtml));
             })
         }
